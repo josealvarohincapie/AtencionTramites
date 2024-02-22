b956efb09d434405a4629f34a438b654bb69c869
ealvarezt <ealvarezt@gmail.com>
2024-02-22 11:39:26 -0500

    Integración Ultimus

---
+++ AtencionTramites.Servicios.Utilidades.Mensajes/HelperMensaje.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports LogWriterHelper
+Imports AtencionTramites.Servicios.Utilidades.Mensajes.Compensar.Servicios.Utilidades.Mensajes
+
+Namespace AtencionTramites.Servicios.Utilidades.Mensajes
+    Module HelperMensaje
+        Function Obtener(ByVal codigo As String) As String
+            Try
+                Return String.Format("{0}|{1}", codigo, CatalogoMensajes.Instancia.GetMensaje(codigo))
+            Catch ex As Exception
+                LogWriter.WriteLog("HelperMensaje.Obtener", "Error al obtener el mensaje del archivo mensajes.xml. Razón: {0} Excepcion:" & ex.Message)
+                Return String.Format("Error al obtener el mensaje del archivo mensajes.xml. Razón: {0}", ex.Message)
+            End Try
+        End Function
+    End Module
+End Namespace
