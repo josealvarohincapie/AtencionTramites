4490087b32c2e8e2aeacbd87a8bc035f1a3f8a47
Mateo Polanco Rodriguez <mateopolanco2@gmail.com>
2024-02-23 10:08:13 -0500

    Modelo. Creacion de ClasificacionPeticionDTO y PeticionarioDTO

--- Modelo/dto/ClasificacionPeticionDTO.vb
+++ Modelo/dto/ClasificacionPeticionDTO.vb
@@ -2,8 +2,75 @@ Namespace Modelo.dto
 
     Public Class ClasificacionPeticionDTO
 
+        Private _tipoPeticion As CatalogoDTO
+        Private _areaDerecho As CatalogoDTO
+        Private _derechos As List(Of CatalogoDTO)
+        Private _descripcionAsesoria As String
+        Private _observaciones As String
+        Private _respuestaEscrita As Boolean
+        Private _conclusionAsesoria As String
 
+        Public Property TipoPeticion As CatalogoDTO
+            Get
+                Return _tipoPeticion
+            End Get
+            Set(value As CatalogoDTO)
+                _tipoPeticion = value
+            End Set
+        End Property
 
+        Public Property AreaDerecho As CatalogoDTO
+            Get
+                Return _areaDerecho
+            End Get
+            Set(value As CatalogoDTO)
+                _areaDerecho = value
+            End Set
+        End Property
 
+        Public Property Derechos As List(Of CatalogoDTO)
+            Get
+                Return _derechos
+            End Get
+            Set(value As List(Of CatalogoDTO))
+                _derechos = value
+            End Set
+        End Property
+
+        Public Property DescripcionAsesoria As String
+            Get
+                Return _descripcionAsesoria
+            End Get
+            Set(value As String)
+                _descripcionAsesoria = value
+            End Set
+        End Property
+
+        Public Property Observaciones As String
+            Get
+                Return _observaciones
+            End Get
+            Set(value As String)
+                _observaciones = value
+            End Set
+        End Property
+
+        Public Property RespuestaEscrita As Boolean
+            Get
+                Return _respuestaEscrita
+            End Get
+            Set(value As Boolean)
+                _respuestaEscrita = value
+            End Set
+        End Property
+
+        Public Property ConclusionAsesoria As String
+            Get
+                Return _conclusionAsesoria
+            End Get
+            Set(value As String)
+                _conclusionAsesoria = value
+            End Set
+        End Property
     End Class
 End Namespace
---
+++ Modelo/dto/PeticionarioDTO.vb
@@ -0,0 +1,184 @@
+﻿Imports Modelo.Modelo.dto
+
+Public Class PeticionarioDTO
+    Private _tipoSolicitante As CatalogoDTO
+    Private _esAnonimo As Boolean
+    Private _tipoDocumentacion As CatalogoDTO
+    Private _numeroIdentificacion As String
+    Private _remitente As String
+    Private _confidencial As Boolean
+    Private _contacto As Boolean
+    Private _correo As String
+    Private _telefono As String
+    Private _direccion As String
+    Private _nacionalidad As CatalogoDTO
+    Private _pais As CatalogoDTO
+    Private _departamento As CatalogoDTO
+    Private _ciudad As CatalogoDTO
+    Private _centroPoblado As CatalogoDTO
+    Private _grupo As CatalogoDTO
+    Private _subGrupo As CatalogoDTO
+    Private _comunidad As String
+
+    Public Property TipoSolicitante As CatalogoDTO
+        Get
+            Return _tipoSolicitante
+        End Get
+        Set(value As CatalogoDTO)
+            _tipoSolicitante = value
+        End Set
+    End Property
+
+    Public Property EsAnonimo As Boolean
+        Get
+            Return _esAnonimo
+        End Get
+        Set(value As Boolean)
+            _esAnonimo = value
+        End Set
+    End Property
+
+    Public Property TipoDocumentacion As CatalogoDTO
+        Get
+            Return _tipoDocumentacion
+        End Get
+        Set(value As CatalogoDTO)
+            _tipoDocumentacion = value
+        End Set
+    End Property
+
+    Public Property NumeroIdentificacion As String
+        Get
+            Return _numeroIdentificacion
+        End Get
+        Set(value As String)
+            _numeroIdentificacion = value
+        End Set
+    End Property
+
+    Public Property Remitente As String
+        Get
+            Return _remitente
+        End Get
+        Set(value As String)
+            _remitente = value
+        End Set
+    End Property
+
+    Public Property Confidencial As Boolean
+        Get
+            Return _confidencial
+        End Get
+        Set(value As Boolean)
+            _confidencial = value
+        End Set
+    End Property
+
+    Public Property Contacto As Boolean
+        Get
+            Return _contacto
+        End Get
+        Set(value As Boolean)
+            _contacto = value
+        End Set
+    End Property
+
+    Public Property Correo As String
+        Get
+            Return _correo
+        End Get
+        Set(value As String)
+            _correo = value
+        End Set
+    End Property
+
+    Public Property Telefono As String
+        Get
+            Return _telefono
+        End Get
+        Set(value As String)
+            _telefono = value
+        End Set
+    End Property
+
+    Public Property Direccion As String
+        Get
+            Return _direccion
+        End Get
+        Set(value As String)
+            _direccion = value
+        End Set
+    End Property
+
+    Public Property Nacionalidad As CatalogoDTO
+        Get
+            Return _nacionalidad
+        End Get
+        Set(value As CatalogoDTO)
+            _nacionalidad = value
+        End Set
+    End Property
+
+    Public Property Pais As CatalogoDTO
+        Get
+            Return _pais
+        End Get
+        Set(value As CatalogoDTO)
+            _pais = value
+        End Set
+    End Property
+
+    Public Property Departamento As CatalogoDTO
+        Get
+            Return _departamento
+        End Get
+        Set(value As CatalogoDTO)
+            _departamento = value
+        End Set
+    End Property
+
+    Public Property Ciudad As CatalogoDTO
+        Get
+            Return _ciudad
+        End Get
+        Set(value As CatalogoDTO)
+            _ciudad = value
+        End Set
+    End Property
+
+    Public Property CentroPoblado As CatalogoDTO
+        Get
+            Return _centroPoblado
+        End Get
+        Set(value As CatalogoDTO)
+            _centroPoblado = value
+        End Set
+    End Property
+
+    Public Property Grupo As CatalogoDTO
+        Get
+            Return _grupo
+        End Get
+        Set(value As CatalogoDTO)
+            _grupo = value
+        End Set
+    End Property
+
+    Public Property SubGrupo As CatalogoDTO
+        Get
+            Return _subGrupo
+        End Get
+        Set(value As CatalogoDTO)
+            _subGrupo = value
+        End Set
+    End Property
+
+    Public Property Comunidad As String
+        Get
+            Return _comunidad
+        End Get
+        Set(value As String)
+            _comunidad = value
+        End Set
+    End Property
+End Class
